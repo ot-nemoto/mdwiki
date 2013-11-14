5c86be4dea56554041582b734dc721d69f335e1d
nemonium <kenichiro.nemoto@gmail.com>
2013-11-14 22:13:58 +0900

    Modified User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,29 @@
 class User
 
-  def self.find(username)
-    USERS[:users].each {|u|
-      next if u[:user] != username
-      return u
-    }
-    return nil
+  def initialize(u)
+    @user = u[:user]
+    @pass = u[:pass]
+    @mail = u[:mail]
   end
 
-  def self.exist(username)
-    return find(username) != nil
+  def user
+    return @user
+  end
+
+  def mail
+    return @mail
+  end
+
+  def auth(pass)
+    return (@pass == nil || @pass == pass)
+  end
+
+  def self.find(user)
+    USERS[:users].each {|u|
+      next if u[:user] != user
+      return User.new(u)
+    }
+    return nil
   end
 
 end
