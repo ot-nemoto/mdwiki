d85a9e6d285664310dcb0efa79c6c7d59fb83304
nemonium <kenichiro.nemoto@gmail.com>
2013-11-16 00:14:54 +0900

    Added a search function

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,10 @@
 class SearchController < ApplicationController
 
   def search(keyword = params[:keyword])
+    @contents = Content.find(keyword).sort {|a,b|
+      # oreder by update_date desc
+      b.update_date <=> a.update_date
+    }
     @header_params = HeaderParams.new(HeaderParams::SEARCH, keyword)
   end
 
