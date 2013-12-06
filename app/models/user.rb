7a26c5bfc587328fea2752ebb7421655e93bd204
nemonium <kenichiro.nemoto@gmail.com>
2013-12-07 02:39:55 +0900

    Incorporate the changes in the develop #14

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User
 
+  DEFAULT_ROLE = RoleModule::READER
+
   def initialize(u = nil)
     if !u.nil?
       @user = u[:user]
       @pass = u[:pass]
       @mail = u[:mail]
+      @role = u[:role]
     end
   end
 
@@ -19,6 +22,11 @@ class User
   def auth?(pass)
     return false if @user.nil? || @user.empty?
     return (@pass.nil? || @pass == pass)
+  end
+
+  def role
+    rt = RoleModule.value_of(@role)
+    return rt.nil? ? DEFAULT_ROLE : rt
   end
 
   def self.find(user)
