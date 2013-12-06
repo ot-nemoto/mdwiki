fae20dd5b2d4e16e9cfa420393c34e9596f1bea5
nemonium <kenichiro.nemoto@gmail.com>
2013-12-07 02:18:16 +0900

    fixed #6

---
+++ lib/assets/role_module.rb
@@ -0,0 +1,21 @@
+module RoleModule
+
+  READER = 0
+  EDITOR = 1
+  ADMIN  = 2
+
+  def self.values
+    return self.constants
+  end
+
+  def self.value_of(name = nil)
+    return nil if name.nil?
+    self.values.each {|v|
+      if v.downcase.to_s == name.downcase.to_s
+        return self.const_get(v)
+      end
+    }
+    return nil
+  end
+
+end
